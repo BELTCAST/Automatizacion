b37f8a689db6e77710e1a8e07288311831ee88e5
Nicolas Beltran <nicolas.belt.cast@gmail.com>
2023-02-23 15:55:54 -0500

    Images ananadido para localizacion de iconos

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import pyautogui 
-pyautogui.position()
+import time 
+
+def spotify_likes_cleaner():
+
+    pyautogui.hotkey("win", "r")
+    time.sleep(2)
+    pyautogui.write('spotify.exe')
+    pyautogui.press('enter')
+    time.sleep(5)
+    biblioteca = pyautogui.locateOnScreen("C:\Users\okins\Documents\Coding\Automatizacion\images\biblioteca_spotify.png")
+    time.sleep(2)
+    print(biblioteca)
+
+
+
+spotify_likes_cleaner()
+
+
+5
